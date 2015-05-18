cf745fb2513f490463c403e394c0451fa4aaf9da
Marcus J. Ellison <marcus@wurkhappy.com>
2015-05-18 12:39:02 -0700

    Initial Commit

---
+++ lil-twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lil-twitter
+//
+//  Created by Marcus J. Ellison on 5/18/15.
+//  Copyright (c) 2015 Marcus J. Ellison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
